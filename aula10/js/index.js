62fd00c860bf53fbc3ab0bdd34db33be2a26c826
Luana Povroznik <luanapovroznik@gmail.com>
2021-07-07 16:36:25 -0300

    nova versão

---
+++ aula10/js/index.js
@@ -0,0 +1,18 @@
+// tipos de dados primitivos 
+
+// String
+const nome = 'Luana'; 
+const nome1 = "Luana";
+const nome2 = `Luana`;
+
+//number
+const num1= 10;
+const num2= 10.33;
+
+let nomeVariavel; // undefined = não aponta para lugar algum na memória  
+let sobrenomeVariavel =  null; // nulo = não aponta para lugar algum na memória
+// (usa para desconfigurar uma variável)
+
+//boolean
+const variavel = true;
+
---
+++ aula9/js/index.js
@@ -0,0 +1,5 @@
+// primeira diferença de var e let
+
+var nome = 'Luiz';
+var nome ='Luana'; // você pode redeclarar uma var, isso não é bom, o let conserta isso.
+console.log(nome);
