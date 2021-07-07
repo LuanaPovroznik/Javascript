549c54ca7c6c5c46901138d9952e532081b478ee
Luana Povroznik <luanapovroznik@gmail.com>
2021-07-07 11:52:15 -0300

    commit new

---
+++ aula8/js/index.js
@@ -0,0 +1,12 @@
+const nome = 'Luana';
+const sobrenome = 'Povroznik';
+const peso = 65;
+const altura = 1.67;
+const idade = 22;
+let imc;
+let anoNascimento;
+let resultImc = peso/(altura*altura);
+let resultNascimento = 2021-22;
+
+console.log(nome + ' ' + sobrenome + ' tem ' + idade + ' anos, pesa ' + peso + ' tem ' + altura + ' de altura e seu IMC é: '+resultImc);
+console.log(nome+' '+sobrenome+' nasceu em: '+resultNascimento);
